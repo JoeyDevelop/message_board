eb76681434180d02ada7fff7a2d9153a7b293cdd
Joey Patterson <joeypatterson177@gmail.com>
2023-04-04 15:32:11 -0400

    Host app on heroku, Connect to postgres db, add prettier/eslint, add/comments, update readme;

---
+++ backend.js
@@ -0,0 +1,33 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+function fetchFromDatabase () {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM messages ORDER BY id ASC', (err, res) => {
+      if (err) {
+        reject(err)
+      } else {
+        const messages = res.rows
+        resolve(messages)
+      }
+    })
+  })
+}
+
+function insertIntoDatabase (message, username, date) {
+  pool.query(`INSERT INTO messages (message, username, date) VALUES ('${message}', '${username}', '${date}')`, (err, res) => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('SUCCESS')
+    }
+  })
+};
+
+module.exports = { fetchFromDatabase, insertIntoDatabase }
--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const { fetchFromDatabase, insertIntoDatabase } = require('../backend')
 
-const dateFormat = () => {
+const express = require('express')
+const router = express.Router()
+
+// FUNCTION TO GET EST DATE AND TIME
+function getFormattedDateTime () {
   const date = new Date()
-  
-  const hours = date.getHours() % 12 || 12;
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const year = date.getFullYear().toString().slice(-2);
-
-  const formattedDate = `${hours}:${minutes} - ${month}/${day}/${year}`;
-  return formattedDate;
+  const timeZone = 'America/New_York' // EST TIME ZONE
+  const options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+    timeZone
+  }
+  const formattedTime = date.toLocaleString('en-US', options)
+  const formattedDate = date.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: '2-digit' })
+  const formattedDateTime = `${formattedTime} - ${formattedDate}`
+  return formattedDateTime
 }
 
-let messages = [
-  {
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elit
-    orci, molestie id erat ut, cursus condimentum sem. Vestibulum feugiat est
-    in ipsum facilisis, at hendrerit lacus cursus. Aenean vel gravida diam, 
-    a laoreet metus. Pellentesque nibh enim, congue id magna a, malesuada ornare 
-    odio. Donec id auctor risus. Phasellus elementum, nulla ac auctor malesuada, 
-    diam tellus mollis mauris, id dapibus mauris leo in lorem. Cras ultricies 
-    ante ut massa auctor viverra. Nunc finibus mauris risus, vitae pulvinar diam 
-    pellentesque quis. Maecenas a quam pharetra, pretium risus nec, efficitur 
-    ipsum. Phasellus quis justo nisl. Morbi non ligula ut tellus suscipit sollicitudin.`,
-    user: 'Amando',
-    added: dateFormat()
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: dateFormat()
-  }
-];
-
+// HANDLE FORM SUBMIT
 router.post('/new', (req, res) => {
-  const message = req.body.text;
-  const user = req.body.user
+  const message = req.body.text
+  const username = req.body.user
+  const date = getFormattedDateTime()
 
-  messages.push({ text: message, user: user, added: dateFormat()})
-  console.log(messages)
+  insertIntoDatabase(message, username, date)
 
   res.redirect('/')
 })
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { messages });
-});
+// GET HOME PAGE
+router.get('/', async function (req, res, next) {
+  try {
+    const messages = await fetchFromDatabase()
+    res.render('index', { messages })
+  } catch (err) {
+    console.error(err)
+    res.render('error', { message: 'Error fetching messages from database.' })
+  }
+})
 
-router.get('/new', function(req, res) {
-  res.render('new');
-}) 
+// GET NEW PAGE
+router.get('/new', function (req, res) {
+  res.render('new')
+})
 
-module.exports = router;
+module.exports = router
