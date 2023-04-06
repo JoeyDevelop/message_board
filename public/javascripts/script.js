e8af44d201db7c3cbee21b135df2f643c0f6e4c9
Joey Patterson <joeypatterson177@gmail.com>
2023-04-06 11:33:25 -0400

    add prettier and eslint

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-const newButton = document.querySelector('.newButton');
+const newButton = document.querySelector('.newButton')
 newButton.addEventListener('click', () => {
-  window.location.href = 'https://message-boardd.herokuapp.com/new';
+  window.location.href = 'https://message-boardd.herokuapp.com/new'
 })
--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,46 @@
-const { fetchFromDatabase, insertIntoDatabase } = require('../backend');
+const { fetchFromDatabase, insertIntoDatabase } = require('../backend')
 
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-function getFormattedDateTime() {
-  const date = new Date();
-  const timeZone = 'America/New_York'; // replace with user's timezone
+function getFormattedDateTime () {
+  const date = new Date()
+  const timeZone = 'America/New_York' // replace with user's timezone
   const options = {
     hour: 'numeric',
     minute: 'numeric',
     hour12: true,
     timeZone
-  };
-  const formattedTime = date.toLocaleString('en-US', options);
-  const formattedDate = date.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: '2-digit' });
-  const formattedDateTime = `${formattedTime} - ${formattedDate}`;
-  return formattedDateTime;
+  }
+  const formattedTime = date.toLocaleString('en-US', options)
+  const formattedDate = date.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: '2-digit' })
+  const formattedDateTime = `${formattedTime} - ${formattedDate}`
+  return formattedDateTime
 }
 
-// let messages = [
-//   {
-//     text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elit
-//     orci, molestie id erat ut, cursus condimentum sem. Vestibulum feugiat est
-//     in ipsum facilisis, at hendrerit lacus cursus. Aenean vel gravida diam, 
-//     a laoreet metus. Pellentesque nibh enim, congue id magna a, malesuada ornare 
-//     odio. Donec id auctor risus. Phasellus elementum, nulla ac auctor malesuada, 
-//     diam tellus mollis mauris, id dapibus mauris leo in lorem. Cras ultricies 
-//     ante ut massa auctor viverra. Nunc finibus mauris risus, vitae pulvinar diam 
-//     pellentesque quis. Maecenas a quam pharetra, pretium risus nec, efficitur 
-//     ipsum. Phasellus quis justo nisl. Morbi non ligula ut tellus suscipit sollicitudin.`,
-//     user: 'Amando',
-//     added: dateFormat()
-//   },
-//   {
-//     text: 'Hello World!',
-//     user: 'Charles',
-//     added: dateFormat()
-//   }
-// ];
-
-// TEST Database connection
-
 router.post('/new', (req, res) => {
-  const message = req.body.text;
+  const message = req.body.text
   const username = req.body.user
   const date = getFormattedDateTime()
 
-  insertIntoDatabase(message, username, date);
+  insertIntoDatabase(message, username, date)
 
   res.redirect('/')
 })
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
-    const messages = await fetchFromDatabase();
-    res.render('index', { messages });
+    const messages = await fetchFromDatabase()
+    res.render('index', { messages })
   } catch (err) {
-    console.error(err);
-    res.render('error', { message: 'Error fetching messages from database.'})
+    console.error(err)
+    res.render('error', { message: 'Error fetching messages from database.' })
   }
-});
+})
 
-router.get('/new', function(req, res) {
-  res.render('new');
-}) 
+router.get('/new', function (req, res) {
+  res.render('new')
+})
 
-module.exports = router;
+module.exports = router
